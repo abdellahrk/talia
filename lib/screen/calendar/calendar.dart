f64095df5abb846ea4cdbd508de8e49a05f53698
Abdellah Ramadan <ramadanabdel24@gmail.com>
2026-08-12 19:53:43 +0100

    add calendar screen and update db schema

--- lib/screen/calendar/calendar.dart
+++ lib/screen/calendar/calendar.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:infinite_calendar_view/infinite_calendar_view.dart';
+import 'package:material_3_expressive/foundations/m3e_theme.dart';
+import 'package:tasks/main.dart';
+import 'package:tasks/model/task.dart';
+import 'package:tasks/service/task_service.dart';
 
 class Calendar extends StatefulWidget {
   const Calendar({super.key});
@@ -8,8 +13,48 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
+  EventsController controller = EventsController();
+
+  @override
+  void initState() {
+    initData();
+    super.initState();
+  }
+
+  void initData() async {
+    final taskService = getIt<TaskService>();
+    await taskService.getUpcomingTasks();
+    List<Event> tasks = [];
+    print("upcoming tasks are: ${taskService.upcomingTasks.value}");
+
+    for (var task in taskService.upcomingTasks.value!) {
+      final event = Event(
+        startTime: DateTime.now(),
+        endTime: task.dueDate,
+        title: task.title,
+        isFullDay: false,
+        columnIndex: 3,
+      );
+      tasks.add(event);
+    }
+
+    controller.updateCalendarData((calendarData) {
+      print("calendar tasks are : $tasks");
+      if (tasks.isNotEmpty) {
+        calendarData.addEvents(tasks);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Calendar")));
+    return Scaffold(
+      // backgroundColor: M3ETheme.of(context).colorScheme.surfaceDim,
+      appBar: AppBar(
+        title: Text("Calendar"),
+        backgroundColor: M3ETheme.of(context).colorScheme.surfaceDim,
+      ),
+      body: EventsMonths(controller: controller),
+    );
   }
 }
--- lib/service/db_service.dart
+++ lib/service/db_service.dart
@@ -11,7 +11,7 @@ class DbService {
     String path = join(dbPath, 'task_db.db');
     database = await openDatabase(
       path,
-      version: 1,
+      version: 2,
       onCreate: (Database db, int version) async {
         await db.execute('''
         CREATE TABLE tasks(
@@ -25,7 +25,31 @@ class DbService {
         )
       ''');
       },
+      onUpgrade: (Database db, int oldVersion, int newVersion) async {
+        Batch batch = db.batch();
+        if (oldVersion == 1) {
+          _addCreatedDateAndTaskList(batch);
+        }
+        await batch.commit();
+      },
     );
+  }
+
+  void _addCreatedDateAndTaskList(Batch batch) {
+    batch.execute('ALTER TABLE tasks ADD COLUMN createdAt TEXT');
+    batch.execute('ALTER TABLE tasks ADD COLUMN isAllDay INTEGER DEFAULT 0');
+    batch.execute('''
+        CREATE TABLE taskItems(
+          id INTEGER PRIMARY KEY AUTOINCREMENT, 
+          taskId INTEGER, 
+          title TEXT,
+          description TEXT DEFAULT NULL,
+          isCompleted INTEGER DEFAULT 0, 
+          createdAt TEXT, 
+          FOREIGN KEY(taskId) REFERENCES tasks(id))''');
+    batch.execute('''
+      CREATE INDEX index_taskItems_taskId ON taskItems(taskId)
+    ''');
   }
 
   Future<int?> addTask(Task task) async {
@@ -55,7 +79,6 @@ class DbService {
   Future<Task?> getTask(int taskId) async {
     List<Map<String, dynamic>> maps = await database.query(
       taskTable,
-      columns: ['id'],
       where: "id = ?",
       whereArgs: [taskId],
     );
